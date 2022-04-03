e5db1084df320264b2c0895e790b561aab6e3e3b
James Benerofe <Jamessbenerofe23@gmail.com>
2022-04-02 17:08:00 -0700

    adding base code

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log(1)
+`strict`
+
+const clear = document.querySelector(`clear`)
+const backspace = document.querySelector(`backspace`)
+const zero = document.querySelector(`zero`)
+const one = document.querySelector(`one`)
+const two = document.querySelector(`two`)
+const three = document.querySelector(`three`)
+const four = document.querySelector(`four`)
+const five = document.querySelector(`five`)
+const size = document.querySelector(`six`)
+const seven = document.querySelector(`seven`)
+const eight = document.querySelector(`eight`)
+const nine = document.querySelector(`nine`)
