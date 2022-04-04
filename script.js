b920d9e0efebe7e9840f5648af4043bebe5c9204
James Benerofe <Jamessbenerofe23@gmail.com>
2022-04-03 17:17:36 -0700

    finished adding css to make it look like calculator.

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 `strict`
+const text = document.querySelector(`.text`);
+const clear = document.getElementById(`clear`);
+const backspace = document.getElementById(`backspace`);
+const zero = document.getElementById(`zero`);
+const one = document.getElementById(`one`);
+const two = document.getElementById(`two`);
+const three = document.getElementById(`three`);
+const four = document.getElementById(`four`);
+const five = document.getElementById(`five`);
+const six = document.getElementById(`six`);
+const seven = document.getElementById(`seven`);
+const eight = document.getElementById(`eight`);
+const nine = document.getElementById(`nine`);
 
-const clear = document.querySelector(`clear`)
-const backspace = document.querySelector(`backspace`)
-const zero = document.querySelector(`zero`)
-const one = document.querySelector(`one`)
-const two = document.querySelector(`two`)
-const three = document.querySelector(`three`)
-const four = document.querySelector(`four`)
-const five = document.querySelector(`five`)
-const size = document.querySelector(`six`)
-const seven = document.querySelector(`seven`)
-const eight = document.querySelector(`eight`)
-const nine = document.querySelector(`nine`)
+text.textContent = `hi`
+clear.addEventListener(`click`, )
+backspace.addEventListener(`click`, )
+zero.addEventListener(`click`, )
+one.addEventListener(`click`, )
+two.addEventListener(`click`, )
+three.addEventListener(`click`, )
+four.addEventListener(`click`, )
+five.addEventListener(`click`, )
+six.addEventListener(`click`, )
+seven.addEventListener(`click`, )
+eight.addEventListener(`click`, )
+nine.addEventListener(`click`, )
+
+function operate(number1, number2, operater) {
+
+}
